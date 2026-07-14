ea0ec0e98e1ea51f043c64615d15b6160aee36c7
Navi Sharma <navisharma7417@gmail.com>
2026-07-14 23:32:02 +0530

    Added Hibernate selective and update/delete projects

---
+++ Hibernate/HIbernateSelective/src/main/java/com/telusko/app/LaunchSelective.java
@@ -0,0 +1,58 @@
+package com.telusko.app;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.telusko.model.Employee1;
+
+public class LaunchSelective {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Employee1.class).buildSessionFactory();
+		Session session = null;
+		
+		Transaction transaction = null;
+		boolean flag = false;
+		
+		try
+		{
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			Employee1 e = new Employee1();
+            e.seteId(1);	
+            e.seteAge(20);
+            e.seteName("Rohan");
+            e.seteCity("BLR");
+            session.persist(e);
+			flag = true;		
+		}
+		catch(HibernateException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		finally
+		{
+			if(flag==true)
+			{
+				transaction.commit();
+			}
+			else
+			{
+				transaction.rollback();
+			}
+			
+			session.close();
+			sessionFactory.close();
+		}
+
+	}
+
+}
---
+++ Hibernate/HIbernateUpdateDelete/src/main/java/com/navi/app/LaunchUpdate.java
@@ -0,0 +1,61 @@
+package com.navi.app;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.navi.model.Student;
+
+
+public class LaunchUpdate 
+{
+
+	public static void main(String[] args)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;	
+		
+		try
+		{
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			Student st = new Student();
+			st.setSid(3);
+			st.setsName("Harsh");
+			st.setsCity("Jaipur ");
+			
+//			session.saveOrUpdate(st);
+			session.merge(st);
+           	flag = true;
+			
+		}
+		catch(HibernateException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		finally
+		{
+			if(flag==true)
+			{
+				transaction.commit();
+			}
+			else
+			{
+				transaction.rollback();
+			}
+			
+			session.close();
+			sessionFactory.close();
+		}
+
+	}
+
+}
