3185eff38896721102b8c9946810880ffe4a5f63
Navi Sharma <navisharma7417@gmail.com>
2026-07-17 23:01:26 +0530

    Added Hibernate HQL retrieval and update/delete projects

---
+++ Hibernate/HIbernateUpdateDeleteHQL/src/main/java/com/navi/app/LaunchUpdate.java
@@ -0,0 +1,61 @@
+package com.navi.app;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.navi.model.Student;
+
+
+public class LaunchUpdate 
+{
+
+	public static void main(String[] args)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;	
+		int count = 0;
+		
+		try
+		{
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+//			count = session.createQuery("UPDATE Student SET sCity= :city").
+//			setParameter("city", "Chennai").executeUpdate();
+			count = session.createMutationQuery("UPDATE Student SET sCity= :city WHERE sName = :name").
+					setParameter("city", "Mumbai").setParameter("name","Raj").executeUpdate();
+           	flag = true;
+			
+		}
+		catch(HibernateException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		finally
+		{
+			if(flag==true)
+			{
+				transaction.commit();
+				System.out.println("No of rows updated "+count);
+			}
+			else
+			{
+				transaction.rollback();
+				System.out.println("No of rows updated "+count);
+			}
+			
+			session.close();
+			sessionFactory.close();
+		}
+
+	}
+
+}
---
+++ Hibernate/HibernateDataRetrievalHQL/src/main/java/com/telusko/app/GetRecordApp.java
@@ -0,0 +1,58 @@
+package com.telusko.app;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.navi.model.Student;
+
+public class GetRecordApp 
+{
+
+	public static void main(String[] args) 
+	{
+	    
+	 	SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).
+		buildSessionFactory();
+	 	Session session1 = null;
+		
+		try
+		{
+			session1 = sessionFactory.openSession();
+			//SELECT * FROM StudentTable WHERE sCity=?;
+//			Query<Student> query = session1.createQuery("From Student WHERE sCity=:city" , Student.class);
+			Query<String> query = session1.createQuery("SELECT sName From Student WHERE sCity IN (:city1, :city2)",String.class);
+			query.setParameter("city1","Bengaluru");
+			query.setParameter("city2","Mumbai");
+			List<String> listStudent = query.list();
+			
+//			for(Student s: listStudent)
+//			{
+//				System.out.println(s);
+//			}
+			
+			listStudent.forEach((s)->System.out.println(s));
+			
+		}
+		catch(HibernateException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		finally
+		{	
+			session1.close();
+			sessionFactory.close();
+		}
+
+	}
+
+}
